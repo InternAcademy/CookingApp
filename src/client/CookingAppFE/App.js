46ad8c4b0f36fc22819de67ac0b762709c9ba1dc
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-06-01 01:33:56 +0300

    move LandingPage to a component folder

--- src/client/CookingAppFE/App.js
+++ src/client/CookingAppFE/App.js
@@ -1,20 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import { StyleSheet, View } from "react-native";
+import LandingPage from "./components/LandingPage";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <Image source={require("./assets/icon.png")} style={styles.logo} />
-      </View>
-      <Text style={styles.title}>Let's Get Started</Text>
-      <Text style={styles.subtitle}>
-        Easy way to manage all your cooking tasks as easy as tapping your finger
-      </Text>
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Get Started</Text>
-      </TouchableOpacity>
+      <LandingPage />
       <StatusBar style="auto" />
     </View>
   );
@@ -24,40 +16,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#f39c12",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 20,
-  },
-  imageContainer: {
-    marginBottom: 30,
-  },
-  logo: {
-    width: 200,
-    height: 200,
-    borderRadius: 100,
-    resizeMode: "contain",
-  },
-  title: {
-    fontSize: 28,
-    fontWeight: "bold",
-    color: "#fff",
-    marginBottom: 10,
-  },
-  subtitle: {
-    fontSize: 16,
-    color: "#fff",
-    textAlign: "center",
-    marginBottom: 30,
-  },
-  button: {
-    backgroundColor: "#fff",
-    paddingVertical: 15,
-    paddingHorizontal: 40,
-    borderRadius: 30,
-  },
-  buttonText: {
-    fontSize: 16,
-    fontWeight: "bold",
-    color: "#f39c12",
   },
 });
