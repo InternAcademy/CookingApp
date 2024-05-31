588ac5fb60df74d782b008283f15b0733ca4d438
Ekrem05 <ekremnptg@gmail.com>
2024-05-31 20:13:04 +0300

    ViewModels folder and necessary classes/records for data transfer added.

---
+++ src/server/CookingApp/ViewModels/Stripe/Product/ProductsResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace CookingApp.ViewModels.Stripe.Product
+{
+    public record ProductsResponse(
+        string Id,
+        string Name,
+        long? Price,
+        string PriceId,
+        string Description,
+        string ImageUrl);
+}
