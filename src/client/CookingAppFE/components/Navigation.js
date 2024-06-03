142080d089623292507c1281510644eb3cf2ce95
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-06-04 01:00:48 +0300

    navigation tailwind update

--- src/client/CookingAppFE/components/Navigation.js
+++ src/client/CookingAppFE/components/Navigation.js
@@ -1,39 +1,48 @@
-// components/Navigation.js
-
 import React from "react";
 import {
   View,
   TouchableOpacity,
   Image,
-  StyleSheet,
   SafeAreaView,
   StatusBar,
   Platform,
 } from "react-native";
+import tw from "twrnc";
 
 const Navigation = () => {
   return (
-    <SafeAreaView style={styles.safeArea}>
+    <SafeAreaView style={tw`bg-white`}>
       <StatusBar barStyle="dark-content" />
-      <View style={styles.navbar}>
-        <Image source={require("../assets/icon.png")} style={styles.logo} />
-        <View style={styles.iconsContainer}>
+      <View
+        style={tw`flex-row justify-between items-center h-15 bg-gray-100 px-4 border-b border-gray-300`}
+      >
+        <Image
+          source={require("../assets/icon.png")}
+          style={tw`w-10 h-10 ml-2`}
+        />
+        <View style={tw`flex-row`}>
           <TouchableOpacity onPress={() => console.log("Back pressed")}>
-            <Image source={require("../assets/back.jpg")} style={styles.icon} />
+            <Image
+              source={require("../assets/back.jpg")}
+              style={tw`w-8 h-8 mx-2`}
+            />
           </TouchableOpacity>
           <TouchableOpacity onPress={() => console.log("Favourites pressed")}>
             <Image
               source={require("../assets/favorite.png")}
-              style={styles.icon}
+              style={tw`w-8 h-8 mx-2`}
             />
           </TouchableOpacity>
           <TouchableOpacity onPress={() => console.log("User pressed")}>
-            <Image source={require("../assets/user.png")} style={styles.icon} />
+            <Image
+              source={require("../assets/user.png")}
+              style={tw`w-8 h-8 mx-2`}
+            />
           </TouchableOpacity>
           <TouchableOpacity onPress={() => console.log("Settings pressed")}>
             <Image
               source={require("../assets/settings.webp")}
-              style={styles.icon}
+              style={tw`w-8 h-8 mx-2`}
             />
           </TouchableOpacity>
         </View>
@@ -41,35 +50,5 @@ const Navigation = () => {
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  safeArea: {
-    backgroundColor: "#FFFFFF",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-  navbar: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    height: 60,
-    backgroundColor: "#F8F8F8",
-    paddingHorizontal: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: "#ddd",
-  },
-  logo: {
-    width: 40,
-    height: 40,
-    marginLeft: 10,
-  },
-  iconsContainer: {
-    flexDirection: "row",
-  },
-  icon: {
-    width: 30,
-    height: 30,
-    marginHorizontal: 10,
-  },
-});
 
 export default Navigation;
