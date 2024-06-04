c5b6336e3a1c1d1c3fe6087514a75da5c5cc8b75
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-06-04 12:11:36 +0300

    revert favourites

--- src/client/CookingAppFE/App.js
+++ src/client/CookingAppFE/App.js
@@ -1,20 +1,16 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
 import { View, ScrollView } from "react-native";
-import Favourite from "./components/Favourite";
 import Navigation from "./components/Navigation";
+import Contacts from "./components/Contacts";
 import tw from "twrnc";
-
-const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <View style={tw`flex-1`}>
       <Navigation />
-      <Stack.Navigator>
-        <Stack.Screen name="Favourite" component={Favourite} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <ScrollView>
+        <Contacts />
+      </ScrollView>
+    </View>
   );
 }
