016fd561a73f5cf1be44ce6c13c5252c5ec215e1
Georgi Markov <gogata1905@gmail.com>
2024-06-20 10:43:38 +0300

    add paperClip

--- src/client/CookingAppFE/components/Home.js
+++ src/client/CookingAppFE/components/Home.js
@@ -1,3 +1,4 @@
+// Home.js
 import React, { useEffect, useState } from 'react';
 import { View, Text, Image, FlatList, TextInput, TouchableOpacity, SafeAreaView, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
@@ -101,6 +102,9 @@ const Home = () => {
       <FlatList data={[{ key: '1' }]} renderItem={renderPost} keyExtractor={item => item.key} contentContainerStyle={tw`flex-grow`} />
       <View style={tw`flex-none flex-row items-center p-2 border-t border-gray-300 bg-white`}>
         <View style={tw`flex-1 flex-row items-center border border-gray-300 rounded-full px-2 mx-1 bg-amber-50`}>
+          <TouchableOpacity onPress={sendMessage} style={tw`p-1`}>
+            <Image source={require('../assets/HomeMessageBar/paperClip.png')} style={tw`w-5 h-5`} />
+          </TouchableOpacity>
           <TextInput style={tw`flex-1 h-10 px-1`} placeholder="Message MealMasterBot" value={message} onChangeText={setMessage} />
           <TouchableOpacity onPress={sendMessage} style={tw`p-1`}>
             <Image source={require('../assets/HomeMessageBar/arrowUpCircle.png')} style={tw`w-6 h-6`} />
