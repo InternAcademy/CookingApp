a9bdbfc492f270615f3ab80dd0a9c705cd0c798a
Georgi Markov <gogata1905@gmail.com>
2024-06-20 08:28:25 +0300

    add error and update thinking

--- src/client/CookingAppFE/components/Thinking.js
+++ src/client/CookingAppFE/components/Thinking.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import { MdComputer } from 'react-icons/md';
+import { View, Text } from 'react-native';
+import tw from 'twrnc';
+import { MaterialIcons } from '@expo/vector-icons'; // Библиотека за икони
 
 const Thinking = () => {
   return (
-    <div className="message">
-      <div className="message__wrapper flex">
-        <div className="message__pic flex items-center justify-center h-8 w-8 bg-slate-200 mx-2 rounded-full text-xl">
-          <MdComputer />
-        </div>
-        <div className="text-left message__createdAt text-sm font-thin italic text-slate-600 dark:text-slate-300">
-          <div className="message__thinking animate-pulse duration-1000 h-12 w-12 dark:text-white text-black text-xl">thinking...</div>
-        </div>
-      </div>
-    </div>
+    <View style={tw`p-4`}>
+      <View style={tw`flex flex-row`}>
+        <View style={tw`flex items-center justify-center h-8 w-8 bg-slate-200 mx-2 rounded-full`}>
+          <MaterialIcons name="computer" size={24} style={tw`text-black`} />
+        </View>
+        <View style={tw`flex-1 text-left`}>
+          <Text style={tw`text-sm font-thin italic text-slate-600 dark:text-slate-300`}>
+            <Text style={tw`animate-pulse text-xl dark:text-white text-black`}>thinking...</Text>
+          </Text>
+        </View>
+      </View>
+    </View>
   );
 };
 
