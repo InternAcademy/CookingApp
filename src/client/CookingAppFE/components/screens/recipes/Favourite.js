13b2736c4b6e690067d40a9661fdeb8a98d20e5c
Georgi Markov <gogata1905@gmail.com>
2024-06-21 10:15:31 +0300

    dark themes

--- src/client/CookingAppFE/components/screens/recipes/Favourite.js
+++ src/client/CookingAppFE/components/screens/recipes/Favourite.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import tw from 'twrnc';
+import { useTheme } from '../../../context/ThemeContext';
 
 const Favourite = () => {
+  const { isDarkTheme } = useTheme();
+
   return (
-    <View style={tw`mx-4`}>
-      <Text style={tw`text-lg font-bold mt-20 mb-2`}>Favorite Recipes</Text>
-      <View style={tw`bg-gray-200 py-4 px-2 rounded-md`}>
-        <Text style={tw`text-base font-medium`}>Lava Cake</Text>
+    <View style={tw`flex-1 mx-4 ${isDarkTheme ? 'bg-[#202020]' : 'bg-white'}`}>
+      <View style={tw`${isDarkTheme ? 'bg-[#202020]' : 'bg-white'}`}>
+        <Text style={tw`text-lg font-bold mt-20 mb-2 ${isDarkTheme ? 'text-white' : 'text-black'}`}>Favorite Recipes</Text>
+      </View>
+      <View style={tw`py-4 px-2 rounded-md bg-white ${isDarkTheme ? 'bg-[#202020]' : 'bg-white'}`}>
+        <Text style={tw`text-base font-medium text-black ${isDarkTheme ? 'text-white' : 'text-black'}`}>Lava Cake</Text>
       </View>
     </View>
   );
