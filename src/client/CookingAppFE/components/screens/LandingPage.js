55757dc39c1a6a3f7bca876d4dd47cdff6adaaa2
Georgi Markov <gogata1905@gmail.com>
2024-06-22 16:46:22 +0300

    newchanges

--- src/client/CookingAppFE/components/screens/LandingPage.js
+++ src/client/CookingAppFE/components/screens/LandingPage.js
@@ -5,6 +5,7 @@ import useAuth from '../../hooks/useAuth';
 import * as WebBrowser from 'expo-web-browser';
 import tw from 'twrnc';
 import { useTheme } from '../../context/ThemeContext';
+import { motion } from 'framer-motion';
 
 const tenantId = process.env.EXPO_PUBLIC_TENANT_ID;
 const clientId = process.env.EXPO_PUBLIC_CLIENT_ID;
@@ -29,14 +30,14 @@ const LandingPage = () => {
   }, [token]);
 
   return (
-    <View style={tw`flex-1 ${isDarkTheme ? 'bg-[#202020]' : 'bg-yellow-500'} items-center justify-center p-94`}>
-      <View style={tw`mb-8`}>
-        <Image source={require('../../assets/Main/icon.png')} style={tw`w-40 h-40 rounded-full`} />
+    <View style={tw` flex-col justify-center items-center bg-yellow-500   w-full h-full `} className=" ">
+      <View style={tw`mb-20`}>
+        <Image source={require('../../assets/Main/icon.png')} style={tw`w-32 h-32 rounded-full`} />
       </View>
       <Text style={tw`text-2xl font-bold ${isDarkTheme ? 'text-white' : 'text-black'} mb-2`}>Let's Get Started</Text>
-      <Text style={tw`text-lg ${isDarkTheme ? 'text-gray-400' : 'text-white'} text-center mb-8`}>Easy way to manage all your cooking tasks as easy as tapping your finger</Text>
-      <TouchableOpacity disabled={!request} title="Login" onPress={() => login()} style={tw`bg-white py-4 px-10 rounded-full`}>
-        <Text style={tw`text-lg font-bold ${isDarkTheme ? 'text-[#202020]' : 'text-yellow-500'}`}>Get Started</Text>
+      <Text style={tw`text-lg px-4 ${isDarkTheme ? 'text-gray-400' : 'text-white'} text-center mb-8`}>Easy way to manage all your cooking tasks as easy as tapping your finger</Text>
+      <TouchableOpacity disabled={!request} title="Login" onPress={() => login()} style={tw`bg-white py-4 px-10  mt-40 -mb-46  rounded-full`}>
+        <Text style={tw`text-lg font-bold  ${isDarkTheme ? 'text-[#202020]' : 'text-yellow-500'}`}>Get Started</Text>
       </TouchableOpacity>
       <Text>{token}</Text>
     </View>
