7a8ee7fde25223c5446867c43832b3fa5542b0db
Georgi Markov <gogata1905@gmail.com>
2024-06-28 01:00:18 +0300

    fix space

--- src/client/CookingAppFE/components/screens/settings/Subscription.js
+++ src/client/CookingAppFE/components/screens/settings/Subscription.js
@@ -29,9 +29,9 @@ const Subscription = () => {
         <Text style={tw`text-3xl font-bold mb-2 ${isDarkTheme ? 'text-white' : 'text-black'}`}>Pricing page title</Text>
         <Text style={tw`text-lg mb-6 ${isDarkTheme ? 'text-gray-400' : 'text-gray-500'}`}>And a subheading describing your pricing plans, too</Text>
 
-        <View style={tw`flex-row w-full px-6 mb-10`}>
+        <View style={tw`flex-row w-full px-6 mb-10 justify-between`}>
           {combinedPlans.map((plan, index) => (
-            <View key={index} style={[tw`flex-1 mb-4 p-4 rounded-lg items-center`, { backgroundColor: isDarkTheme ? '#303030' : plan.color }]}>
+            <View key={index} style={[tw`flex-1 p-4 rounded-lg items-center`, { backgroundColor: isDarkTheme ? '#303030' : plan.color }, index !== 0 && tw`ml-2`]}>
               <Text style={tw`text-lg font-semibold mb-2 ${isDarkTheme ? 'text-white' : 'text-black'}`}>{plan.name}</Text>
               <Text style={tw`text-2xl font-bold mb-4 ${isDarkTheme ? 'text-white' : 'text-black'}`}>
                 {plan.price} <Text style={tw`text-base font-normal ${isDarkTheme ? 'text-white' : 'text-black'}`}>{plan.name === 'Plan 3' ? 'per year' : 'per month'}</Text>
