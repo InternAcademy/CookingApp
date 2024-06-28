7800dcb043fc754c6a96f37e0bd4d49882e55882
Ekrem05 <ekremnptg@gmail.com>
2024-06-28 20:05:56 +0300

    React Query and Redux implemented

--- src/client/CookingAppFE/components/screens/settings/Subscription.js
+++ src/client/CookingAppFE/components/screens/settings/Subscription.js
@@ -1,65 +1,216 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
-import tw from 'twrnc';
-import { useTheme } from '../../../context/ThemeContext';
-
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  Linking,
+} from "react-native";
+import tw from "twrnc";
+import { jwtDecode } from "jwt-decode";
+import { useQuery, useMutation } from "@tanstack/react-query";
+import { useTheme } from "../../../context/ThemeContext";
+import { fetchSubs, createSub } from "../../../http/subs";
+import { useSelector } from "react-redux";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 const Subscription = () => {
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ["subs"],
+    queryFn: async () => {
+      const token = await AsyncStorage.getItem("token");
+      return fetchSubs(token);
+    },
+  });
+  const {
+    mutate,
+    isPending: isSubscribing,
+    isError: isSubError,
+    error: subError,
+  } = useMutation({
+    mutationFn: createSub,
+    onSuccess: (data) => {
+      console.log(data);
+      Linking.openURL(data.data.invoiceUrl).catch((err) => {
+        Alert.alert("Error", `Failed to open URL: ${err.message}`);
+      });
+      queryClient.invalidateQueries("subs");
+    },
+  });
   const { isDarkTheme } = useTheme();
   const [openFaqIndex, setOpenFaqIndex] = useState(null);
 
-  const combinedPlans = [
-    { name: 'Plan 1', price: 'Free', period: 'per month', features: ['Feature', 'Feature', 'Feature', 'Feature'], color: '#FFF4E2' },
-    { name: 'Plan 2', price: '$5', period: 'per month', features: ['Feature', 'Feature', 'Feature', 'Feature'], color: '#FFE1AF' },
-    { name: 'Plan 3', price: '$50', period: 'per year', features: ['Feature', 'Feature', 'Feature', 'Feature'], color: '#FFD48D' }
-  ];
+  async function handleSelection(id) {
+    console.log("hello");
+    const token = await AsyncStorage.getItem("token");
+    const cred = jwtDecode(token);
+    console.log(cred.preferred_username);
+    mutate({ token: token, email: cred.preferred_username, priceId: id });
+  }
 
   const faqs = [
-    { question: "What's the most frequently asked question?", answer: 'Answer the frequently asked question in a simple sentence, a longish paragraph, or even in a list.' },
-    { question: 'How about a second one?', answer: 'Answer for the second question.' },
-    { question: 'And a third?', answer: 'Answer for the third question.' }
+    {
+      question: "What's the most frequently asked question?",
+      answer:
+        "Answer the frequently asked question in a simple sentence, a longish paragraph, or even in a list.",
+    },
+    {
+      question: "How about a second one?",
+      answer: "Answer for the second question.",
+    },
+    { question: "And a third?", answer: "Answer for the third question." },
   ];
 
-  const toggleFaq = index => {
+  const toggleFaq = (index) => {
     setOpenFaqIndex(openFaqIndex === index ? null : index);
   };
 
   return (
-    <ScrollView style={tw`flex-1 ${isDarkTheme ? 'bg-[#202020]' : 'bg-white'} mt-10`}>
+    <ScrollView
+      style={tw`flex-1 ${isDarkTheme ? "bg-[#202020]" : "bg-white"} mt-10`}
+    >
       <View style={tw`flex-1 items-center p-6`}>
-        <Text style={tw`text-3xl font-bold mb-2 ${isDarkTheme ? 'text-white' : 'text-black'}`}>Pricing page title</Text>
-        <Text style={tw`text-lg mb-6 ${isDarkTheme ? 'text-gray-400' : 'text-gray-500'}`}>And a subheading describing your pricing plans, too</Text>
+        <Text
+          style={tw`text-3xl font-bold mb-2 ${isDarkTheme ? "text-white" : "text-black"}`}
+        >
+          {isSubscribing ? "Subscribing..." : "Pricing page title"}
+        </Text>
+        <Text
+          style={tw`text-lg mb-6 ${isDarkTheme ? "text-gray-400" : "text-gray-500"}`}
+        >
+          And a subheading describing your pricing plans, too
+        </Text>
 
         <View style={tw`flex-row w-full px-6 mb-10 justify-between`}>
-          {combinedPlans.map((plan, index) => (
-            <View key={index} style={[tw`flex-1 p-4 rounded-lg items-center`, { backgroundColor: isDarkTheme ? '#303030' : plan.color }, index !== 0 && tw`ml-2`]}>
-              <Text style={tw`text-lg font-semibold mb-2 ${isDarkTheme ? 'text-white' : 'text-black'}`}>{plan.name}</Text>
-              <Text style={tw`text-2xl font-bold ${isDarkTheme ? 'text-white' : 'text-black'}`}>{plan.price}</Text>
-              <Text style={tw`text-sm font-normal mb-4 ${isDarkTheme ? 'text-white' : 'text-black'}`}>{plan.period}</Text>
-              <View style={tw`mb-4`}>
-                {plan.features.map((feature, idx) => (
-                  <Text key={idx} style={tw`text-xs mb-1 ${isDarkTheme ? 'text-white' : 'text-black'}`}>
-                    • {feature}
-                  </Text>
-                ))}
-              </View>
-              <TouchableOpacity style={tw`px-4 py-2 bg-yellow-500 rounded-full`}>
-                <Text style={tw`text-base font-medium text-white text-xs`}>Select</Text>
-              </TouchableOpacity>
+          {isPending && (
+            <View style={tw`mb-4`}>
+              <Text
+                style={tw`text-xs mb-1 ${isDarkTheme ? "text-white" : "text-black"}`}
+              >
+                fetching...
+              </Text>
             </View>
-          ))}
+          )}
+          {data && (
+            <>
+              {/* Free sub */}
+              <View
+                style={[
+                  tw`flex-1 p-4 rounded-lg items-center`,
+                  { backgroundColor: isDarkTheme ? "#303030" : "#FFF4E2" },
+                ]}
+              >
+                <Text
+                  style={tw`text-lg font-semibold mb-2 ${isDarkTheme ? "text-white" : "text-black"}`}
+                >
+                  Meal Master
+                </Text>
+                <Text
+                  style={tw`text-2xl font-bold ${isDarkTheme ? "text-white" : "text-black"}`}
+                >
+                  Free
+                </Text>
+                <Text
+                  style={tw`text-sm font-normal mb-4 ${isDarkTheme ? "text-white" : "text-black"}`}
+                >
+                  one time
+                </Text>
+                <View style={tw`mb-4`}>
+                  <Text
+                    style={tw`text-xs mb-1 ${isDarkTheme ? "text-white" : "text-black"}`}
+                  >
+                    • Limited chats
+                  </Text>
+                </View>
+                <TouchableOpacity
+                  style={tw`px-4 py-2 bg-yellow-500 rounded-full`}
+                >
+                  <Text style={tw`text-base font-medium text-white text-xs`}>
+                    Current
+                  </Text>
+                </TouchableOpacity>
+              </View>
+
+              {/* Subscriptions */}
+              {data.data.map((sub) => (
+                <View
+                  key={sub.id}
+                  style={[
+                    tw`flex-1 p-4 rounded-lg items-center`,
+                    { backgroundColor: isDarkTheme ? "#303030" : "#FFF4E2" },
+                  ]}
+                >
+                  <Text
+                    style={tw`text-lg font-semibold mb-2 ${isDarkTheme ? "text-white" : "text-black"}`}
+                  >
+                    {sub.name}
+                  </Text>
+                  <Text
+                    style={tw`text-2xl font-bold ${isDarkTheme ? "text-white" : "text-black"}`}
+                  >
+                    {sub.price / 100}$
+                  </Text>
+                  <Text
+                    style={tw`text-sm font-normal mb-4 ${isDarkTheme ? "text-white" : "text-black"}`}
+                  >
+                    {sub.period === "year" ? "per year" : "per month"}
+                  </Text>
+                  <View style={tw`mb-4`}>
+                    <Text
+                      style={tw`text-xs mb-1 ${isDarkTheme ? "text-white" : "text-black"}`}
+                    >
+                      • Unlimited chats
+                    </Text>
+                    <Text
+                      style={tw`text-xs mb-1 ${isDarkTheme ? "text-white" : "text-black"}`}
+                    >
+                      • Unlimited chats
+                    </Text>
+                  </View>
+                  <TouchableOpacity
+                    style={tw`px-4 py-2 bg-yellow-500 rounded-full`}
+                  >
+                    <Text
+                      style={tw`text-base font-medium text-white text-xs`}
+                      onPress={() => handleSelection(sub.priceId)}
+                    >
+                      Select
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              ))}
+            </>
+          )}
         </View>
         <View style={tw`w-full items-start px-6`}>
-          <Text style={tw`text-2xl font-bold mb-4 ${isDarkTheme ? 'text-white' : 'text-black'}`}>Heading for FAQs</Text>
+          <Text
+            style={tw`text-2xl font-bold mb-4 ${isDarkTheme ? "text-white" : "text-black"}`}
+          >
+            Heading for FAQs
+          </Text>
         </View>
         {faqs.map((faq, index) => (
           <View key={index} style={tw`mb-6 w-full px-6`}>
             <TouchableOpacity onPress={() => toggleFaq(index)}>
               <View style={tw`flex-row justify-between items-center`}>
-                <Text style={tw`text-lg font-semibold ${isDarkTheme ? 'text-white' : 'text-black'}`}>{faq.question}</Text>
-                <Text style={tw`text-2xl font-semibold ${isDarkTheme ? 'text-white' : 'text-black'}`}>{openFaqIndex === index ? '-' : '+'}</Text>
+                <Text
+                  style={tw`text-lg font-semibold ${isDarkTheme ? "text-white" : "text-black"}`}
+                >
+                  {faq.question}
+                </Text>
+                <Text
+                  style={tw`text-2xl font-semibold ${isDarkTheme ? "text-white" : "text-black"}`}
+                >
+                  {openFaqIndex === index ? "-" : "+"}
+                </Text>
               </View>
             </TouchableOpacity>
-            {openFaqIndex === index && <Text style={tw`text-base mt-2 ${isDarkTheme ? 'text-gray-400' : 'text-gray-600'}`}>{faq.answer}</Text>}
+            {openFaqIndex === index && (
+              <Text
+                style={tw`text-base mt-2 ${isDarkTheme ? "text-gray-400" : "text-gray-600"}`}
+              >
+                {faq.answer}
+              </Text>
+            )}
           </View>
         ))}
       </View>
