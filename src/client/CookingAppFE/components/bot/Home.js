af6b13b1449d7defa9dddcaa5f5868845936e26b
Georgi Markov <gogata1905@gmail.com>
2024-07-06 23:41:26 +0300

    square size

--- src/client/CookingAppFE/components/bot/Home.js
+++ src/client/CookingAppFE/components/bot/Home.js
@@ -73,7 +73,7 @@ const Home = () => {
       <FlatList data={[{ key: '1' }]} renderItem={renderPost} keyExtractor={item => item.key} contentContainerStyle={tw`flex-grow`} />
       {photoUri && (
         <View style={tw`flex w-full flex-row justify-center items-center mb-5 relative`}>
-          <Image source={{ uri: photoUri }} style={tw`w-16 h-16 rounded-full mr-2`} />
+          <Image source={{ uri: photoUri }} style={tw`w-16 h-16`} />
           <TouchableOpacity style={tw`absolute right-0 p-1`} onPress={handleRemovePhoto}>
             <Ionicons name="close-circle" size={24} color="red" />
           </TouchableOpacity>
