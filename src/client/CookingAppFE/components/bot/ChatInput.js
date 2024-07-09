e399a96a72f19fcd82df295eeeacd5fd397296ad
Georgi Markov <gogata1905@gmail.com>
2024-07-09 03:47:15 +0300

    changed 4 components to work with both string and image - and 1 component with edited brackets

--- src/client/CookingAppFE/components/bot/ChatInput.js
+++ src/client/CookingAppFE/components/bot/ChatInput.js
@@ -1,11 +1,11 @@
+import { useState } from "react";
 import { View, Image, TextInput, TouchableOpacity } from "react-native";
 import tw from "twrnc";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-
+import * as ImagePicker from "expo-image-picker";
 import { useSelector, useDispatch } from "react-redux";
 import { uiActions } from "../../redux/uiSlice";
 import { userActions } from "../../redux/userSlice";
-
 import useChatMutation from "../../hooks/useNewChat";
 import useContinueChatMutation from "../../hooks/useKeepChatting";
 
@@ -16,36 +16,73 @@ export default function ChatInput() {
   const dispatch = useDispatch();
   const { initialMessage, isPending, isError, error } = useChatMutation();
   const { keepChatting, isChatting, isChatError, chatError } = useContinueChatMutation();
+  const [image, setImage] = useState(null);
 
   async function sendMessage() {
     const token = await AsyncStorage.getItem("token");
-    const newMessage = { role: "user", content: input };
-
     if (!selectedChat) {
-      initialMessage({ token: token, message: input });
-      dispatch(
-        userActions.selectChat({
-          ...selectedChat,
-          content: [newMessage]
-        })
-      );
+      if (image) {
+        initialMessage({ token: token, messageType: "Image", image: image });
+        dispatch(
+          userActions.selectChat({
+            ...selectedChat,
+            content: [{ role: "user", content: image.uri }]
+          })
+        );
+        setImage(null);
+      } else {
+        initialMessage({ token: token, messageType: "Text", content: input });
+        dispatch(
+          userActions.selectChat({
+            ...selectedChat,
+            content: [{ role: "user", content: input }]
+          })
+        );
+      }
     } else {
-      keepChatting({ token: token, chatId: selectedChat.id, message: input });
-      dispatch(
-        userActions.selectChat({
-          ...selectedChat,
-          content: [...selectedChat.content, newMessage]
-        })
-      );
+      if (image) {
+        keepChatting({ token: token, chatId: selectedChat.id, messageType: "Image", image: image });
+        dispatch(
+          userActions.selectChat({
+            ...selectedChat,
+            content: [...selectedChat.content, { role: "user", content: image.uri }]
+          })
+        );
+        setImage(null);
+      } else {
+        keepChatting({ token: token, chatId: selectedChat.id, messageType: "Text", content: input });
+        dispatch(
+          userActions.selectChat({
+            ...selectedChat,
+            content: [...selectedChat.content, { role: "user", content: input }]
+          })
+        );
+      }
+    }
+    dispatch(uiActions.setInput(""));
+  }
+
+  async function pickImage() {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0]);
     }
   }
+
   function handleTyping(value) {
     dispatch(uiActions.setInput(value));
   }
+
   return (
     <>
       <View style={tw`flex w-6/8 flex-row justify-center items-center border ${isDarkTheme ? "border-gray-700 bg-gray-900" : "border-gray-300 bg-amber-50"} rounded-full px-2 mx-1`}>
-        <TouchableOpacity onPress={sendMessage} style={tw`p-1`}>
+        <TouchableOpacity onPress={pickImage} style={tw`p-1`}>
           <Image source={require("../../assets/HomeMessageBar/paperClip.png")} style={tw`w-5 h-5 ${isDarkTheme ? "tint-white" : ""}`} />
         </TouchableOpacity>
         <TextInput style={tw`flex-1 h-10 px-1 ${isDarkTheme ? "text-white" : "text-black"}`} placeholder="Message MealMasterBot" placeholderTextColor={isDarkTheme ? "gray" : "black"} value={input} onChangeText={handleTyping} />
--- src/client/CookingAppFE/http/chat.js
+++ src/client/CookingAppFE/http/chat.js
@@ -1,64 +1,108 @@
 const ip = process.env.EXPO_PUBLIC_PERSONAL_IP;
 
-export async function continueChat({ token, chatId, message }) {
+export async function continueChat({ token, chatId, messageType, content, image }) {
+  let body;
+  let headers = {
+    Authorization: `Bearer ${token}`
+  };
+
+  if (messageType === "Text") {
+    headers["Content-Type"] = "application/json";
+    body = JSON.stringify({
+      chatId: chatId,
+      type: messageType,
+      content: content
+    });
+  } else if (messageType === "Image") {
+    body = new FormData();
+    body.append("chatId", chatId);
+    body.append("type", messageType);
+    body.append("image", {
+      uri: image.uri,
+      name: image.name,
+      type: image.type
+    });
+  }
+
   const response = await fetch(`${ip}/continue`, {
     method: "POST",
-    headers: {
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      chatId: chatId,
-      message: message,
-    }),
+    headers: headers,
+    body: body
   });
+
   if (!response.ok) {
-    throw new Error(response.errors);
+    throw new Error(response.statusText);
   }
-  const body = await response.json();
-  return body;
+
+  const responseBody = await response.json();
+  return responseBody;
 }
-export async function newChat({ token, message }) {
-  console.log(message);
+
+export async function newChat({ token, messageType, content, image }) {
+  console.log({ messageType, content, image });
+  let body;
+  let headers = {
+    Authorization: `Bearer ${token}`
+  };
+
+  if (messageType === "Text") {
+    headers["Content-Type"] = "application/json";
+    body = JSON.stringify({
+      type: messageType,
+      content: content
+    });
+  } else if (messageType === "Image") {
+    body = new FormData();
+    body.append("type", messageType);
+    body.append("image", {
+      uri: image.uri,
+      name: image.name,
+      type: image.type
+    });
+  }
+
   const response = await fetch(`${ip}/new-chat`, {
     method: "POST",
-    headers: {
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      message: message,
-    }),
+    headers: headers,
+    body: body
   });
+
   if (!response.ok) {
-    throw new Error(response.errors);
+    throw new Error(response.statusText);
   }
+
   const data = await response.json();
   return data;
 }
+
 export async function getChat({ token, chatId }) {
   const response = await fetch(`${ip}/c/${chatId}`, {
     headers: {
       Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json",
-    },
+      "Content-Type": "application/json"
+    }
   });
+
   if (!response.ok) {
-    throw new Error(response.errors);
+    throw new Error(response.statusText);
   }
+
   const data = await response.json();
   return data;
 }
+
 export async function getUserChats({ token, userId }) {
   const response = await fetch(`${ip}/user-chats/${userId}`, {
     headers: {
       Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json",
-    },
+      "Content-Type": "application/json"
+    }
   });
+
   if (!response.ok) {
-    throw new Error(response.errors);
+    throw new Error(response.statusText);
   }
+
   const data = await response.json();
   return data;
 }
