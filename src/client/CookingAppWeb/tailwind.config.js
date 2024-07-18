061973ba033d7c6dcd9a5161776e16f1d33659ae
Georgi Markov <gogata1905@gmail.com>
2024-07-18 10:18:04 +0300

    add some main colors

--- src/client/CookingAppWeb/tailwind.config.js
+++ src/client/CookingAppWeb/tailwind.config.js
@@ -7,10 +7,17 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        customWhite: "#FFFFFF", // light theme - Basic white
+        customGray: "#202020", // dark theme - Dark gray background
+        darkGray: "#303030", // Dark gray color for background and dividers in dark theme
+        lightGray: "#F3F3F3", // Light gray color for background and dividers in light theme - Light gray
+
+        customYellow: "#FFEB3B", // for highlights and buttons - Basic yellow
+        customBlack: "#000000", // text and icons - Basic black
+        customOrange: "#FFA500", // Basic orange color for highlights and warnings
+        darkOrange: "#FF8C00", // Dark orange color for highlights and critical warnings
+        darkYellow: "#FFD700", // Dark yellow color for accents and secondary buttons
       },
     },
     screens: {
