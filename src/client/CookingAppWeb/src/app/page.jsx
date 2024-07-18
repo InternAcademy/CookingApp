7a8b808b5e1c470c6073ba555dbe2bd8627b0ff9
Georgi Markov <gogata1905@gmail.com>
2024-07-18 18:57:30 +0300

    rename from .js to .jsx

---
+++ src/client/CookingAppWeb/src/app/page.jsx
@@ -0,0 +1,10 @@
+// main.jsx
+import LandingPage from "./components/LandingPage/LandingPage";
+
+export default function Main() {
+  return (
+    <main className="flex items-center justify-center flex-col h-screen w-screen">
+      <LandingPage />
+    </main>
+  );
+}
