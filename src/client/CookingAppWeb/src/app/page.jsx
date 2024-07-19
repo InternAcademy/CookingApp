1e036b6948231f7e4898d6b122723d64cc131a15
Georgi Markov <gogata1905@gmail.com>
2024-07-19 11:12:44 +0300

    home.jsx

--- src/client/CookingAppWeb/src/app/page.jsx
+++ src/client/CookingAppWeb/src/app/page.jsx
@@ -1,11 +1,12 @@
-import LandingPage from "./components/LandingPage";
+// import LandingPage from "./components/LandingPage";
+import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 
 export default function App() {
   return (
     <main className="flex items-center justify-center flex-col h-screen w-screen">
       <NavBar />
-      <LandingPage />
+      <Home />
     </main>
   );
 }
