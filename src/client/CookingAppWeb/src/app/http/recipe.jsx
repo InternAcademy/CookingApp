363470e221e0f5e7a6e6d6cde12afe149c09f5c9
Georgi Markov <gogata1905@gmail.com>
2024-07-20 21:26:18 +0300

    http

---
+++ src/client/CookingAppWeb/src/app/http/recipe.jsx
@@ -0,0 +1,85 @@
+"use client";
+export async function createRecipe({ token, request }) {
+  const response = await fetch(`${ip}/create-recipe`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json"
+    },
+
+    body: `"${request}"`
+  });
+  if (!response.ok) {
+    throw new Error("Resource not found");
+  }
+
+  const responseBody = await response.json();
+
+  return responseBody.data;
+}
+
+export async function getRecipes({ token, userId }) {
+  const response = await fetch(`localhost:3000/recipes/${userId}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json"
+    }
+  });
+  if (!response.ok) {
+    throw new Error("Resource not found");
+  }
+  const responseBody = await response.json();
+
+  return responseBody.data;
+}
+
+export async function getRecipeById({ token, recipeId }) {
+  console.log(recipeId);
+  const response = await fetch(`localhost:3000/r/${recipeId}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json"
+    }
+  });
+  if (!response.ok) {
+    throw new Error("Resource not found");
+  }
+  const responseBody = await response.json();
+  console.log(responseBody);
+  return responseBody.data;
+}
+
+export async function archive({ token, recipeId }) {
+  const response = await fetch(`localhost:3000/archive/${recipeId}`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json"
+    }
+  });
+  if (!response.ok) {
+    throw new Error("Resource not found");
+  }
+
+  const responseBody = await response.json();
+
+  return responseBody.data;
+}
+export async function getArchivedRecipes({ token, userId }) {
+  const response = await fetch(`localhost:3000/archived-recipes/${userId}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json"
+    }
+  });
+  if (!response.ok) {
+    throw new Error("Resource not found");
+  }
+
+  const responseBody = await response.json();
+
+  return responseBody.data;
+}
---
+++ src/client/CookingAppWeb/src/app/http/subs.jsx
@@ -0,0 +1,35 @@
+"use client";
+export async function fetchSubs(token) {
+  const response = await fetch(`localhost:3000/api/stripe/products`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json"
+    }
+  });
+  if (!response.ok) {
+    throw new Error(response.errors);
+  }
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
+export async function createSub({ token, email, priceId }) {
+  const response = await fetch(`localhost:3000/api/stripe/subscription`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      email: email,
+      priceId: priceId
+    })
+  });
+  console.log(response);
+  if (!response.ok) {
+    throw new Error(response.errors);
+  }
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
