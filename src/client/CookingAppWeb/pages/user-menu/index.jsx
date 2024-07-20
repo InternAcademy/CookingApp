60cdd7dfad1af95b30c093a97bdbd187dc2c4379
Georgi Markov <gogata1905@gmail.com>
2024-07-20 18:00:45 +0300

    add space bw icons and text

--- src/client/CookingAppWeb/pages/user-menu/index.jsx
+++ src/client/CookingAppWeb/pages/user-menu/index.jsx
@@ -18,42 +18,42 @@ const UserMenu = () => {
 
       <div className="flex flex-col items-start w-full space-y-6">
         <div className="flex items-center w-full mb-2 cursor-pointer" onClick={() => router.push("/recipes")} title="Recipes">
-          <FaUtensils className="w-6 h-6 mr-3" color={isDarkTheme ? "white" : "black"} />
+          <FaUtensils className="w-6 h-6 mr-4" color={isDarkTheme ? "white" : "black"} />
           <span className={`text-lg ${isDarkTheme ? "text-white" : "text-black"}`}>Recipes</span>
         </div>
         <div className="flex items-center w-full mb-2 cursor-pointer" onClick={() => router.push("/subscription")} title="Subscription">
-          <FaCreditCard className="w-6 h-6 mr-3" color={isDarkTheme ? "white" : "black"} />
+          <FaCreditCard className="w-6 h-6 mr-4" color={isDarkTheme ? "white" : "black"} />
           <span className={`text-lg ${isDarkTheme ? "text-white" : "text-black"}`}>Subscription</span>
         </div>
         <div className="flex items-center w-full mb-2 cursor-pointer" onClick={() => router.push("/food-preferences")} title="Food Preferences">
-          <FaLeaf className="w-6 h-6 mr-3" color={isDarkTheme ? "white" : "black"} />
+          <FaLeaf className="w-6 h-6 mr-4" color={isDarkTheme ? "white" : "black"} />
           <span className={`text-lg ${isDarkTheme ? "text-white" : "text-black"}`}>Food Preferences</span>
         </div>
         <div className="flex items-center w-full mb-2 cursor-pointer" onClick={() => router.push("/archived-recipes")} title="Archived Recipes">
-          <FaArchive className="w-6 h-6 mr-3" color={isDarkTheme ? "white" : "black"} />
+          <FaArchive className="w-6 h-6 mr-4" color={isDarkTheme ? "white" : "black"} />
           <span className={`text-lg ${isDarkTheme ? "text-white" : "text-black"}`}>Archived Recipes</span>
         </div>
         <div className="flex items-center w-full mb-2 cursor-pointer" onClick={() => router.push("/language-theme")} title="Language & Theme">
-          <FaLanguage className="w-6 h-6 mr-3" color={isDarkTheme ? "white" : "black"} />
+          <FaLanguage className="w-6 h-6 mr-4" color={isDarkTheme ? "white" : "black"} />
           <span className={`text-lg ${isDarkTheme ? "text-white" : "text-black"}`}>Language & Theme</span>
         </div>
         <div className="flex items-center w-full mb-2 cursor-pointer" onClick={() => router.push("/rules-policies")} title="Rules And Policies">
-          <FaFileAlt className="w-6 h-6 mr-3" color={isDarkTheme ? "white" : "black"} />
+          <FaFileAlt className="w-6 h-6 mr-4" color={isDarkTheme ? "white" : "black"} />
           <span className={`text-lg ${isDarkTheme ? "text-white" : "text-black"}`}>Rules And Policies</span>
         </div>
         <div className="flex items-center w-full mb-2 cursor-pointer" onClick={() => router.push("/about")} title="About">
-          <FaInfoCircle className="w-6 h-6 mr-3" color={isDarkTheme ? "white" : "black"} />
+          <FaInfoCircle className="w-6 h-6 mr-4" color={isDarkTheme ? "white" : "black"} />
           <span className={`text-lg ${isDarkTheme ? "text-white" : "text-black"}`}>About</span>
         </div>
         <div className="flex items-center w-full mb-2 cursor-pointer" onClick={() => router.push("/contacts")} title="Contacts">
-          <FaEnvelope className="w-6 h-6 mr-3" color={isDarkTheme ? "white" : "black"} />
+          <FaEnvelope className="w-6 h-6 mr-4" color={isDarkTheme ? "white" : "black"} />
           <span className={`text-lg ${isDarkTheme ? "text-white" : "text-black"}`}>Contacts</span>
         </div>
       </div>
 
       <div className="mt-auto mb-4">
         <button onClick={() => console.log("Sign Out")} className="flex items-center" title="Sign Out">
-          <FaSignOutAlt className="w-6 h-6 mr-3" color={isDarkTheme ? "white" : "black"} />
+          <FaSignOutAlt className="w-6 h-6 mr-4" color={isDarkTheme ? "white" : "black"} />
           <span className={`text-lg ${isDarkTheme ? "text-gray-100" : "text-black"}`}>Sign Out</span>
         </button>
       </div>
