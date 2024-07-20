48d4d1bc76161372691663d50e5e6eec12ca66bf
Georgi Markov <gogata1905@gmail.com>
2024-07-20 08:26:42 +0300

    rename routes

---
+++ src/client/CookingAppWeb/pages/rules-policies/index.jsx
@@ -0,0 +1,12 @@
+// pages/RulesAndPolicies.jsx
+import React from "react";
+
+const RulesAndPolicies = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-2xl font-bold text-gray-900">RulesAndPolicies</h1>
+    </div>
+  );
+};
+
+export default RulesAndPolicies;
