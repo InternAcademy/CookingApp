1bb55290a2c83c3aa3b80811512454ce1bd8e9d1
Georgi Markov <gogata1905@gmail.com>
2024-07-21 12:36:44 +0300

    change contacts

--- src/client/CookingAppWeb/pages/contacts/index.jsx
+++ src/client/CookingAppWeb/pages/contacts/index.jsx
@@ -10,17 +10,17 @@ const Contacts = () => {
   const isDarkTheme = theme === "dark";
 
   return (
-    <div className={`flex flex-col p-5 pt-20 min-h-screen ${isDarkTheme ? "bg-[#202020]" : "bg-white"}`}>
+    <div className={`flex flex-col p-5 pt-20 min-h-screen ${isDarkTheme ? "bg-customGray" : "bg-customWhite"}`}>
       <div className="flex flex-col mb-5 w-full">
-        <label className={`text-lg ${isDarkTheme ? "text-white" : "text-gray-800"} mb-2`}>Subject</label>
-        <input type="text" className={`h-12 border ${isDarkTheme ? "border-gray-600 bg-gray-700 text-white" : "border-yellow-500 bg-yellow-500 text-black"} rounded-full px-3 w-full`} placeholder="What would you like to talk about?" style={{ color: isDarkTheme ? "#888" : "#555" }} />
+        <label className={`text-lg ${isDarkTheme ? "text-customWhite" : "text-gray-800"} mb-2`}>Subject</label>
+        <input type="text" className={`h-12 border ${isDarkTheme ? "border-customGray600 bg-gray-700 text-customWhite" : "border-customOrange bg-customOrange text-black"} rounded-full px-3 w-full`} placeholder="What would you like to talk about?" style={{ color: isDarkTheme ? "#888" : "#555" }} />
       </div>
       <div className="flex flex-col mb-5 w-full">
-        <label className={`text-lg ${isDarkTheme ? "text-white" : "text-gray-800"} mb-2`}>Message</label>
-        <textarea className={`h-64 border ${isDarkTheme ? "border-gray-600 bg-gray-700 text-white" : "border-yellow-500 bg-yellow-500 text-black"} rounded-lg px-3 pt-2 w-full`} placeholder="Enter your message here..." style={{ color: isDarkTheme ? "#888" : "#555" }} rows="10" />
+        <label className={`text-lg ${isDarkTheme ? "text-customWhite" : "text-gray-800"} mb-2`}>Message</label>
+        <textarea className={`h-64 border ${isDarkTheme ? "border-customGray600 bg-gray-700 text-customWhite" : "border-customOrange bg-customOrange text-black"} rounded-lg px-3 pt-2 w-full`} placeholder="Enter your message here..." style={{ color: isDarkTheme ? "#888" : "#555" }} rows="10" />
       </div>
-      <button className={`h-12 flex justify-center items-center rounded-full ${isDarkTheme ? "bg-gray-600" : "bg-yellow-500"} w-full`}>
-        <span className={`text-lg ${isDarkTheme ? "text-white" : "text-gray-800"}`}>Send</span>
+      <button className={`h-12 flex justify-center items-center rounded-full ${isDarkTheme ? "bg-gray-600" : "bg-customOrange"} w-full`}>
+        <span className={`text-lg ${isDarkTheme ? "text-customWhite" : "text-gray-800"}`}>Send</span>
       </button>
     </div>
   );
