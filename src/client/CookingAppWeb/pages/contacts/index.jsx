f42e253c9a2565b3defd60cff0c8b27abba0c168
Georgi Markov <gogata1905@gmail.com>
2024-07-21 13:17:09 +0300

    add title contacts

--- src/client/CookingAppWeb/pages/contacts/index.jsx
+++ src/client/CookingAppWeb/pages/contacts/index.jsx
@@ -11,6 +11,8 @@ const Contacts = () => {
 
   return (
     <div className={`flex flex-col p-5 pt-20 min-h-screen ${isDarkTheme ? "bg-customGray" : "bg-customWhite"}`}>
+      <h1 className={`text-2xl font-semibold mb-8 ${isDarkTheme ? "text-customWhite" : "text-gray-800"}`}>Contacts</h1>
+
       <div className="flex flex-col mb-5 w-full">
         <label className={`text-lg ${isDarkTheme ? "text-customWhite" : "text-gray-800"} mb-2`}>Subject</label>
         <input type="text" className={`h-12 border ${isDarkTheme ? "border-customGray600 bg-gray-700 text-customWhite" : "border-customOrange bg-customOrange text-black"} rounded-full px-3 w-full`} placeholder="What would you like to talk about?" style={{ color: isDarkTheme ? "#888" : "#555" }} />
