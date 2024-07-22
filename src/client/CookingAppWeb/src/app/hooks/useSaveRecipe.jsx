ef59553a7f0bec6c2d9f4c367489d217c030de69
Georgi Markov <gogata1905@gmail.com>
2024-07-22 16:00:22 +0300

    add hooks

---
+++ src/client/CookingAppWeb/src/app/hooks/useSaveRecipe.jsx
@@ -0,0 +1,36 @@
+import { useMutation } from "@tanstack/react-query";
+import { useDispatch, useSelector } from "react-redux";
+import { uiActions } from "../redux/uiSlice";
+import { createRecipe } from "../http/recipe";
+
+const useSaveRecipe = () => {
+  const dispatch = useDispatch();
+  const chat = useSelector(state => state.user.selectedChat);
+  const lastMessageContent = chat && chat.content.length > 0 ? chat.content[chat.content.length - 1].content : null;
+
+  const {
+    mutate: save,
+    isLoading,
+    isError,
+    error
+  } = useMutation({
+    mutationKey: ["recipe", lastMessageContent],
+    mutationFn: createRecipe,
+    onMutate: () => {},
+    onSuccess: response => {
+      console.log(response);
+    },
+    onError: error => {
+      dispatch(uiActions.setResponseError(error.message));
+    }
+  });
+
+  return {
+    save,
+    isLoading,
+    isError,
+    error
+  };
+};
+
+export default useSaveRecipe;
