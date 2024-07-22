abb6dfdd9d5d5439fd41f8f393b32f9f4223a707
Georgi Markov <gogata1905@gmail.com>
2024-07-22 17:00:41 +0300

    RecipesDetails page

---
+++ src/client/CookingAppWeb/pages/recipes-details/index.jsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from "react";
+import { useTheme } from "next-themes";
+import "tailwindcss/tailwind.css";
+
+const RecipesDetails = () => {
+  const { theme } = useTheme();
+  const isDarkTheme = theme === "dark";
+
+  return (
+    <div className={`flex items-center justify-center min-h-screen ${isDarkTheme ? "bg-[#202020]" : "bg-gray-100"}`}>
+      <h1 className={`text-2xl font-bold ${isDarkTheme ? "text-white" : "text-gray-900"}`}>RecipesDetails</h1>
+    </div>
+  );
+};
+
+export default RecipesDetails;
