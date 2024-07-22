56e18617b145aac22c2567278dab8a89f5acd15f
Georgi Markov <gogata1905@gmail.com>
2024-07-22 16:58:08 +0300

    chat error added

---
+++ src/client/CookingAppWeb/src/app/components/bot/ChatError.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { FaLaptop } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import "tailwindcss/tailwind.css";
+
+const ChatError = ({ message }) => {
+  const isDarkTheme = useSelector(state => state.ui.isDarkTheme);
+
+  return (
+    <div className="p-4 bg-red-600 shadow-lg rounded-lg m-4">
+      <div className="flex flex-row items-center">
+        <FaLaptop size={24} className={isDarkTheme ? "text-white" : "text-black"} />
+        <span className={`ml-2 text-sm font-semibold italic ${isDarkTheme ? "text-white" : "text-black"}`}>{message}</span>
+      </div>
+    </div>
+  );
+};
+
+export default ChatError;
