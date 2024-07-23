6e08d7e814d39277695e937de5e9d989331168d7
Panayotsky-dev <panayotsky.dev@gmail.com>
2024-07-24 00:40:18 +0300

    Update HeaderButton.js
    +Changing design of the token button to be like chatgpt
    -not connected to backend to listen for new messages and track how much messages you have left
    -not connected to know if you are subscribed or not

--- src/client/CookingAppFE/components/bot/HeaderButton.js
+++ src/client/CookingAppFE/components/bot/HeaderButton.js
@@ -3,13 +3,15 @@ import React, { useState, useEffect } from 'react';
 import { TouchableOpacity, Text } from 'react-native';
 import tw from 'twrnc';
 import * as Animatable from 'react-native-animatable';
+import { useSelector } from 'react-redux';
 
 const HeaderButton = () => {
   const [buttonText, setButtonText] = useState('Вземи PLUS');
+  const isDarkTheme = useSelector(state => state.ui.isDarkTheme);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setButtonText(prev => prev === 'Вземи PLUS' ? 'Нов текст' : 'Вземи PLUS');
+      setButtonText(prev => prev === 'Вземете PLUS ' ? '3 Tokens left' : 'Вземете PLUS ');
     }, 3000); // Промяна на текста на всеки 3 секунди, идеята ми е да видим как ще изглежда.
     return () => clearInterval(interval);
   }, []);
@@ -20,12 +22,12 @@ const HeaderButton = () => {
   };
 
   return (
-    <TouchableOpacity onPress={handlePress} style={tw`bg-blue-500 py-2 px-4 rounded-full`}>
+    <TouchableOpacity onPress={handlePress} style={tw`mt-1 ml-2 `}>
       <Animatable.Text
-        animation="slideInRight"
-        duration={400}
+        animation="slideInDown"
+        duration={300}
         key={buttonText} // Промяна на ключа при промяна на текста, за да се активира анимацията
-        style={tw`text-white font-bold`}
+        style={tw`border font-medium py-2 w-34 text-center rounded-[10px] ${isDarkTheme ? "  text-white border-slate-100/10 bg-slate-100/10 " : "border-black text-black"}     `}
       >
         {buttonText}
       </Animatable.Text>
