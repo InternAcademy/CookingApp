39081e54bf4b9d7178245aa82938801441b294ea
Mihail Donchev <mihaildonchev88@abv.bg>
2024-07-23 23:19:47 +0300

    add food-preferences style

--- src/client/CookingAppWeb/src/app/contacts/page.jsx
+++ src/client/CookingAppWeb/src/app/contacts/page.jsx
@@ -18,36 +18,36 @@ const Contacts = () => {
         <input
           type="text"
           className={`h-12 border placeholder-slate-500 ${isDarkTheme
-            ? "border-customGray600 bg-gray-700 text-customWhite"
-            : "border-customOrange bg-customOrange text-black"} 
+            ? "border-customGray600 bg-gray-700 text-customWhite placeholder:text-#fff"
+            : "border-customOrange bg-customOrange text-black placeholder:text-#555"} 
         rounded-full px-3 w-full 
         smallPhone:px-5 smallPhone:h-12 smallPhone:text-sm 
-        phone:px-6 phone:h-14 phone:text-md 
-        table:px-7 tablet:h-16 tablet:text-lg 
-        web:px-7 web:h-14 web:text-md`}
-          placeholder="What would you like to talk about?" style={{ color: isDarkTheme ? "#888" : "#555" }} />
+        phone:px-6 phone:h-14 phone:text-sm 
+        table:px-7 tablet:h-16 tablet:text-base 
+        web:px-7 web:h-14 web:text-base`}
+          placeholder="What would you like to talk about?"/>
       </div>
       <div className="flex flex-col mb-5 w-full">
         <label className={`text-lg ${isDarkTheme ? "text-customWhite" : "text-gray-800"} mb-2`}>Message</label>
         <textarea
           className={`h-64 border placeholder-slate-500 ${isDarkTheme
-            ? "border-customGray600 bg-gray-700 text-customWhite"
-            : "border-customOrange bg-customOrange text-black"} 
+            ? "border-customGray600 bg-gray-700 text-customWhite placeholder:text-#fff"
+            : "border-customOrange bg-customOrange text-black placeholder:text-#555"} 
         rounded-lg px-3 pt-2 w-full 
         smallPhone:px-5 smallPhone:pt-4 smallPhone:text-sm 
-        phone:px-6 phone:pt-5 phone:text-md tablet:px-7 
-        tablet:pt-6 tablet:text-lg 
-        web:px-7 web:pt-4 web:text-md`}
-          placeholder="Enter your message here..." style={{ color: isDarkTheme ? "#888" : "#555" }} rows="10" />
+        phone:px-6 phone:pt-5 phone:text-sm 
+        tablet:px-7 tablet:pt-6 tablet:text-base 
+        web:px-7 web:pt-4 web:text-base`}
+          placeholder="Enter your message here..." rows="10" />
       </div>
       <button
         className={`h-12 flex justify-center items-center rounded-full transition duration-300 ease-in-out ${isDarkTheme
           ? "bg-gray-600 border-customWhite border hover:border-gray-400 hover:text-gray-400"
           : "bg-customOrange border hover:border-gray-800 hover:text-gray-800 hover:shadow-lg"} w-full 
-        smallPhone:h-14 smallPhone:text-xl 
-        phone:h-16 phone:text-md 
-        tablet:h-18 tablet:text-lg 
-        web:h-14 web:text-md`}>
+        smallPhone:h-14 smallPhone:text-sm 
+        phone:h-16 phone:text-sm 
+        tablet:h-18 tablet:text-base 
+        web:h-14 web:text-base`}>
         <span
           className={`text-lg ${isDarkTheme ? "text-customWhite hover:text-gray-400" : "text-gray-800 hover:text-gray-800"}`}>Send</span>
       </button>
--- src/client/CookingAppWeb/src/app/food-preferences/page.jsx
+++ src/client/CookingAppWeb/src/app/food-preferences/page.jsx
@@ -89,7 +89,7 @@ const FoodPreferences = () => {
   }
 
   return (
-    <div className={`${isDarkTheme ? "bg-gray-900" : "bg-white"} min-h-screen p-6`}>
+    <div className={`${isDarkTheme ? "bg-customGray" : "bg-customWhite"} min-h-screen p-6`}>
       <div className="flex flex-col items-center">
         <h1 className={`text-xl font-semibold mb-4 ${isDarkTheme ? "text-white" : "text-black"}`}>Food Preferences</h1>
 
@@ -97,10 +97,10 @@ const FoodPreferences = () => {
           className={`w-full mb-6 pb-6 ${isDarkTheme
             ? "bg-gray-800"
             : "bg-gray-200"} rounded-xl py-4 px-4 
-        smallPhone:py-5 smallPhone:px-5 
-        phone:py-6 phone:px-6 
-        tablet:py-7 tablet:px-7 
-        web:py-8 web:px-8`}>
+        smallPhone:py-5 smallPhone:px-5 smallPhone:text-sm
+        phone:py-6 phone:px-6 phone:text-sm
+        tablet:py-7 tablet:px-7 tablet:text-base
+        web:py-8 web:px-8 web:text-base`}>
           <h2 className={`text-lg font-semibold mb-4 ${isDarkTheme ? "text-white" : "text-black"}`}>Allergens</h2>
           {alergens.length > 0 ? (
             <div className="flex flex-wrap mb-4">
@@ -109,11 +109,12 @@ const FoodPreferences = () => {
                   key={index}
                   onClick={() => handleRemoveAlergen(index)}
                   className={`border flex items-center rounded-full px-3 py-1 mx-1 mb-2 
-                    phone:px-3 phone:py-1.5 phone:text-base 
-                    tablet:px-4 tablet:py-2 tablet:text-lg 
+                    phone:px-3 phone:py-1.5 phone:text-sm 
+                    tablet:px-4 tablet:py-2 tablet:text-base 
+                    web:px-3 web:py-1 web:text-base
                     ${isDarkTheme
-                      ? "border-amber-200 text-white"
-                      : "border-gray-300 bg-white shadow-md text-black"}`}>
+                      ? "border-blue-500 text-blue-300 bg-gray-800 hover:bg-gray-700 shadow-xl hover:shadow-2xl"
+                      : "border-gray-300 bg-white text-black shadow-md hover:border-gray-400 hover:bg-gray-100 hover:shadow-lg"}`}>
                   {alergen}
                   <IoClose className="ml-2" />
                 </button>
@@ -128,8 +129,9 @@ const FoodPreferences = () => {
             onChange={e => setAlergenInput(e.target.value)}
             onKeyDown={handleAddAlergenPressEnter}
             className={`border rounded-lg px-4 py-2 mb-2 w-full 
-          phone:px-4 phone:py-2 phone:text-base 
-          tablet:px-4 tablet:py-2 tablet:text-lg 
+          phone:px-4 phone:py-2 phone:text-sm 
+          tablet:px-4 tablet:py-2 tablet:text-base 
+          web:text-base
           ${isDarkTheme
                 ? "border-gray-600 bg-gray-900 text-white"
                 : "border-gray-300 bg-white text-black"}`}
@@ -143,11 +145,11 @@ const FoodPreferences = () => {
           <button onClick={handleAddAlergen} className="w-full flex items-center justify-center mt-4 transition duration-300 ease-in-out">
             <div
               className={`w-84 px-14 py-3 bg-customOrange rounded-full flex items-center justify-center border text-sm 
-              phone:px-14 phone:py-3 phone:text-base 
-              tablet:px-16 tablet:py-3.5 tablet:text-lg 
-              web:px-18 web:py-4 web:text-xl 
+              phone:px-14 phone:py-3 phone:text-sm 
+              tablet:px-16 tablet:py-3.5 tablet:text-base 
+              web:px-18 web:py-4 web:px-18 web:text-base 
               ${isDarkTheme
-                  ? "border-gray-600 hover:border-gray-400 hover:text-gray-400"
+                  ? "border-gray-600 text-white hover:border-gray-400 hover:text-gray-400 hover:shadow-lg"
                   : "border-gray-300 hover:border-gray-800 hover:text-gray-800 hover:shadow-lg"}`}>
               <p className="text-black text-center text-base font-medium">Add Allergen</p>
             </div>
@@ -161,9 +163,9 @@ const FoodPreferences = () => {
             ? "bg-gray-800"
             : "bg-gray-200"} rounded-xl py-4 px-4
         smallPhone:py-5 smallPhone:px-5 
-        phone:py-6 phone:px-6 
-        tablet:py-7 tablet:px-7 
-        web:py-8 web:px-8`}>
+        phone:py-6 phone:px-6 phone:text-sm
+        tablet:py-7 tablet:px-7 tablet:text-base
+        web:py-8 web:px-8 web:text-base`}>
           <h2 className={`text-lg font-semibold mb-4 ${isDarkTheme ? "text-white" : "text-black"}`}>Disliked Foods</h2>
           {foodPreferences.length > 0 ? (
             <div className="flex flex-wrap mb-4">
@@ -172,12 +174,12 @@ const FoodPreferences = () => {
                   key={index}
                   onClick={() => handleRemoveFoodPreference(index)}
                   className={`border flex items-center rounded-full px-3 py-1 mx-1 mb-2 ${isDarkTheme
-                    ? "border-gray-600 text-white"
-                    : "border-gray-300 bg-white shadow-md text-black"}
-                    // phone:px-3 phone:py-1.5 phone:text-base 
-                    // tablet:px-4 tablet:py-2 tablet:text-lg
-                    `
-                    }>
+                    ? "border-blue-500 text-blue-300 bg-gray-800 hover:bg-gray-700 shadow-xl hover:shadow-2xl"
+                    : "border-gray-300 bg-white text-black shadow-md hover:border-gray-400 hover:bg-gray-100 hover:shadow-lg"}
+                    phone:px-3 phone:py-1.5 phone:text-sm 
+                    tablet:px-4 tablet:py-2 tablet:text-base
+                    web:px-3 web:py-1 web:text-base
+                    `}>
                   {preference}
                   <IoClose className="ml-2" />
                 </button>
@@ -192,8 +194,9 @@ const FoodPreferences = () => {
             onChange={e => setFoodPreferenceInput(e.target.value)}
             onKeyDown={handleAddDislikeFoodsPressEnter}
             className={`border rounded-lg px-4 py-2 mb-2 w-full 
-            phone:px-4 phone:py-2 phone:text-base 
-            tablet:px-4 tablet:py-2 tablet:text-lg  
+            phone:px-4 phone:py-2 phone:text-sm 
+            tablet:px-4 tablet:py-2 tablet:text-base  
+            web:text-base
           ${isDarkTheme
                 ? "border-gray-600 bg-gray-900 text-white"
                 : "border-gray-300 bg-white text-black"}`}
@@ -201,16 +204,30 @@ const FoodPreferences = () => {
           {foodError && <p className="text-red-500 mb-2 text-center">{foodError}</p>}
           <button
             onClick={handleAddFoodPreference}
-            className="w-full flex items-center justify-center mt-2 transition duration-300 ease-in-out
-          phone:px-14 phone:py-3 phone:text-base 
-              tablet:px-16 tablet:py-3.5 tablet:text-lg 
-              web:px-20 web:py-6 web:text-xl">
-            <div className={`w-84 px-16 py-3 bg-customOrange rounded-full flex items-center justify-center border ${isDarkTheme ? "border-gray-600 hover:border-gray-400 hover:text-gray-400" : "border-gray-300 hover:border-gray-800 hover:text-gray-800 hover:shadow-lg"}`}>
+            className="w-full flex items-center justify-center mt-2 transition duration-300 ease-in-out">
+            <div
+              className={`w-84 px-16 py-3 my-3 bg-customOrange rounded-full flex items-center justify-center border ${isDarkTheme
+                ? "border-gray-600 hover:border-gray-400 hover:text-gray-400"
+                : "border-gray-300 hover:border-gray-800 hover:text-gray-800 hover:shadow-lg"}
+            phone:px-18 phone:py-3 phone:text-sm 
+            tablet:px-20 tablet:py-3.5 tablet:text-base 
+            web:px-20 web:py-4 web:text-base
+            `}>
               <p className="text-black text-center text-base font-medium">Add Food</p>
             </div>
           </button>
           <div className="mt-4">
-            <select value={selectedPreference} onChange={e => setSelectedPreference(e.target.value)} className={`border rounded-lg px-4 py-2 w-full ${isDarkTheme ? "border-gray-600 bg-gray-900 text-white" : "border-gray-300 bg-white text-black"}`}>
+            <select 
+            value={selectedPreference} 
+            onChange={e => setSelectedPreference(e.target.value)} 
+            className={`border rounded-lg px-4 py-2 w-full ${isDarkTheme 
+            ? "border-gray-600 bg-gray-900 text-white" 
+            : "border-gray-300 bg-white text-black"}
+            text-sm smallPhone:px-4 smallPhone:py-2.5 
+            phone:px-4 phone:py-2.5 phone:text-sm 
+            tablet:px-5 tablet:py-3 tablet:text-base 
+            web:px-6 web:py-3.5 web:text-base
+            `}>
               <option value="none">None</option>
               <option value="vegetarian">Vegetarian</option>
               <option value="vegan">Vegan</option>
@@ -221,7 +238,11 @@ const FoodPreferences = () => {
         <button onClick={handleSavePreferences} className="w-full flex items-center justify-center mt-4 mb-8 transition duration-300 ease-in-out">
           <div className={`w-84 px-16 py-3 bg-green-500 rounded-full flex items-center justify-center border ${isDarkTheme
             ? "border-gray-600 hover:border-gray-400 hover:text-gray-400"
-            : "border-gray-300 hover:border-gray-800 hover:text-gray-800 hover:shadow-lg"}`}>
+            : "border-gray-300 hover:border-gray-800 hover:text-gray-800 hover:shadow-lg"}
+            phone:px-18 phone:py-3 phone:text-sm 
+            tablet:px-20 tablet:py-3.5 tablet:text-base 
+            web:px-18 web:py-4 web:px-20 web:text-base
+            `}>
             <p className="text-center text-base font-medium text-slate-50">Save Food</p>
           </div>
         </button>
