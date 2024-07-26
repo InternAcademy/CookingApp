18dacee8a0bf679c2aea06a5b800a000c0e10303
Georgi Markov <gogata1905@gmail.com>
2024-07-26 21:41:20 +0300

    add icon to Sign Out

--- src/client/CookingAppWeb/src/components/auth/SignOutButton.jsx
+++ src/client/CookingAppWeb/src/components/auth/SignOutButton.jsx
@@ -1,11 +1,18 @@
-'use client'
+// SignOutButton.jsx
+"use client";
 import { handleLogout } from "@/msal/msal";
+import { FaSignOutAlt } from "react-icons/fa";
+import { useTheme } from "next-themes";
 
 const SignOutButton = () => {
+  const { isDarkTheme } = useTheme();
+
   return (
     <button className="flex items-center" title="Sign Out" onClick={() => handleLogout("redirect")}>
-    <span className={`text-lg text-black`}>Sign Out</span>
-  </button>
+      <FaSignOutAlt className="w-6 h-6 mr-4" color={isDarkTheme ? "white" : "black"} />
+      <span className={`text-lg ${isDarkTheme ? "text-gray-100" : "text-black"}`}>Sign Out</span>
+    </button>
   );
 };
+
 export default SignOutButton;
