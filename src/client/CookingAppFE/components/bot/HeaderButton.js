b73ba726387061319988ebce962c9b3d5fafc40c
Panayotsky-dev <panayotsky.dev@gmail.com>
2024-07-26 18:28:00 +0300

    Updating NavBar Possition of icons
    +fixed token button to be in the middle
    +changed position of the left and right sided icons to have some space of "breating"

--- src/client/CookingAppFE/components/bot/HeaderButton.js
+++ src/client/CookingAppFE/components/bot/HeaderButton.js
@@ -8,13 +8,14 @@ import { useSelector } from 'react-redux';
 const HeaderButton = () => {
   const [buttonText, setButtonText] = useState('Вземи PLUS');
   const isDarkTheme = useSelector(state => state.ui.isDarkTheme);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setButtonText(prev => prev === 'Вземете PLUS ' ? '3 Tokens left' : 'Вземете PLUS ');
-    }, 3000); // Промяна на текста на всеки 3 секунди, идеята ми е да видим как ще изглежда.
-    return () => clearInterval(interval);
-  }, []);
+  
+  //! animation loop
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setButtonText(prev => prev === 'Вземете PLUS ' ? '3 Tokens left' : 'Вземете PLUS ');
+  //   }, 3000); // Промяна на текста на всеки 3 секунди, идеята ми е да видим как ще изглежда при промяна.
+  //   return () => clearInterval(interval);
+  // }, []);
 
   const handlePress = () => {
     
@@ -22,12 +23,12 @@ const HeaderButton = () => {
   };
 
   return (
-    <TouchableOpacity onPress={handlePress} style={tw`mt-1 ml-2 `}>
+    <TouchableOpacity onPress={handlePress} style={tw`mt-1 `}>
       <Animatable.Text
         animation="slideInDown"
         duration={300}
         key={buttonText} // Промяна на ключа при промяна на текста, за да се активира анимацията
-        style={tw`border font-medium py-2 w-34 text-center rounded-[10px] ${isDarkTheme ? "  text-white border-slate-100/10 bg-slate-100/10 " : "border-black text-black"}     `}
+        style={tw`border flex  font-medium py-2 w-34 text-center rounded-[10px] ${isDarkTheme ? "  text-white border-slate-100/10 bg-slate-100/10 " : "border-black text-black"}     `}
       >
         {buttonText}
       </Animatable.Text>
--- src/client/CookingAppFE/components/navigation/NavBar.js
+++ src/client/CookingAppFE/components/navigation/NavBar.js
@@ -25,22 +25,24 @@ const NavBar = () => {
     <SafeAreaView style={tw`absolute top-0 left-0 right-0 z-1 w-full ${isDarkTheme ? "bg-[#202020]" : "bg-white"}`}>
       <StatusBar barStyle={isDarkTheme ? "light-content" : "dark-content"} />
       <View style={tw`flex-row justify-between items-center h-15 w-full px-4 ${isDarkTheme ? "bg-[#202020]" : "bg-white"}`}>
-        <View style={tw`flex-row items-center`}>
-          <TouchableOpacity onPress={() => setOpen(true)} style={tw`mx-2`}>
+        <View style={tw`flex-row items-center justify-start  w-1/3`}>
+          
+          <TouchableOpacity onPress={() => setOpen(true)} style={tw` `}>
             <Bars3BottomLeftIcon size={24} color={isDarkTheme ? "white" : "black"} />
           </TouchableOpacity>
-          <TouchableOpacity onPress={startNewChat} style={tw`mx-0 mt-1`}>
+          <TouchableOpacity onPress={startNewChat} style={tw`mt-1 pl-4`}>
             <ChatBubbleBottomCenterTextIcon size={24} color={isDarkTheme ? "white" : "black"} />
           </TouchableOpacity>
+          
         </View>
-        <View style={tw`flex-1 flex-row justify-center items-center`}>
+        <View style={tw`flex-1 w-2/3  flex-row justify-center items-center pl-2`}>
           <HeaderButton />
         </View>
-        <View style={tw`flex-row items-center mr-4`}>
-          <TouchableOpacity onPress={() => navigation.navigate("Recipes")} style={tw`mx-2`}>
+        <View style={tw`flex-row justify-end items-center mr-4 w-1/3 `}>
+          <TouchableOpacity onPress={() => navigation.navigate("Recipes")} style={tw` pr-4`}>
             <Ionicons name="restaurant" size={24} color={isDarkTheme ? "white" : "black"} />
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigation.navigate("UserMenu")} style={tw`mx-2`}>
+          <TouchableOpacity onPress={() => navigation.navigate("UserMenu")} style={tw` -mr-3 `}>
             <Ionicons name="person" size={24} color={isDarkTheme ? "white" : "black"} />
           </TouchableOpacity>
         </View>
