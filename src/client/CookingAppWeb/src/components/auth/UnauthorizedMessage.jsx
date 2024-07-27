fe265dff7ddc4130f078ddf643a13bb565cc7b4f
Georgi Markov <gogata1905@gmail.com>
2024-07-27 16:24:52 +0300

    change text style

--- src/client/CookingAppWeb/src/components/auth/UnauthorizedMessage.jsx
+++ src/client/CookingAppWeb/src/components/auth/UnauthorizedMessage.jsx
@@ -7,7 +7,7 @@ export default function UnauthorizedMessage() {
       <div className="mb-8">
         <Image src="/Main/icon2_dark.png" alt="Icon" width={100} height={100} />
       </div>
-      <h1 className={`text-2xl font-bold text-customWhite mb-2`}>Let's Get Started</h1>
+      <h1 className={`text-2xl font-bold text-customWhite mb-2`}>{"Let's Get Started"}</h1>
       <p className={`text-lg px-4 text-customWhite text-center mb-8`}>Easy way to manage all your cooking tasks as easy as tapping your finger</p>
       <SignInButton text="Get Started" />
     </div>
