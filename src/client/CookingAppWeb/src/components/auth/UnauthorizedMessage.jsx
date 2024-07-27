0a94624b08aa4ab7a6857738a59e8f3f7050856c
Georgi Markov <gogata1905@gmail.com>
2024-07-27 19:55:12 +0300

    rearrange home section

--- src/client/CookingAppWeb/src/components/auth/UnauthorizedMessage.jsx
+++ src/client/CookingAppWeb/src/components/auth/UnauthorizedMessage.jsx
@@ -5,7 +5,6 @@ import { useRef } from "react";
 
 export default function UnauthorizedMessage() {
   const downloadRef = useRef(null);
-  const recipesRef = useRef(null);
 
   const scrollToDownload = () => {
     if (downloadRef.current) {
@@ -15,33 +14,34 @@ export default function UnauthorizedMessage() {
 
   return (
     <div className="flex flex-col justify-center items-center bg-customOrange w-full min-h-screen relative">
-      <div className="absolute top-0 w-full flex justify-between items-center p-4 bg-customOrange">
-        <div className="flex items-center">
-          <Image src="/Main/icon2_dark.png" alt="Logo" width={50} height={50} />
-          <span className="text-customWhite text-xl ml-2">MealMasterBot</span>
-        </div>
-        <nav className="flex space-x-6">
-          <a href="#" className="text-customWhite hover:text-customGray400">
-            Home
-          </a>
-          <a href="#recipes" className="text-customWhite hover:text-customGray400">
-            Recipes
-          </a>
-          <a href="#" className="text-customWhite hover:text-customGray400" onClick={scrollToDownload}>
-            Download
-          </a>
-          <a href="#" className="text-customWhite hover:text-customGray400">
-            About Us
-          </a>
-        </nav>
-      </div>
-      <div className="text-center max-w-2xl mx-auto mt-40">
-        <h1 className="text-5xl font-bold text-customWhite mb-6">The Easiest Way To Make Your Favorite Meal</h1>
-        <p className="text-lg text-customWhite mb-12">Discover 1000+ recipes in your hand with the best recipe. Help you to find the easiest way to cook.</p>
-        <SignInButton text="Get Started" />
-      </div>
       {/* Home Section */}
-      <div className="flex flex-col justify-center items-center bg-customOrange w-full min-h-screen">{/* Content for Home Section */}</div>
+      <div className="relative w-full min-h-screen flex flex-col justify-center items-center">
+        <div className="absolute top-0 w-full flex justify-between items-center p-4 bg-customOrange">
+          <div className="flex items-center">
+            <Image src="/Main/icon2_dark.png" alt="Logo" width={50} height={50} />
+            <span className="text-customWhite text-xl ml-2">MealMasterBot</span>
+          </div>
+          <nav className="flex space-x-6">
+            <a href="#" className="text-customWhite hover:text-customGray400">
+              Home
+            </a>
+            <a href="#recipes" className="text-customWhite hover:text-customGray400">
+              Recipes
+            </a>
+            <a href="#" className="text-customWhite hover:text-customGray400" onClick={scrollToDownload}>
+              Download
+            </a>
+            <a href="#" className="text-customWhite hover:text-customGray400">
+              About Us
+            </a>
+          </nav>
+        </div>
+        <div className="text-center max-w-2xl mx-auto mt-40">
+          <h1 className="text-5xl font-bold text-customWhite mb-6">The Easiest Way To Make Your Favorite Meal</h1>
+          <p className="text-lg text-customWhite mb-12">Discover 1000+ recipes in your hand with the best recipe. Help you to find the easiest way to cook.</p>
+          <SignInButton text="Get Started" />
+        </div>
+      </div>
       {/* Recipes Section */}
       <div id="recipes" className="relative w-full min-h-screen">
         <Image src="/Main/Recipes.jpg" alt="Recipes Background" layout="fill" objectFit="cover" quality={100} className="absolute top-0 left-0 w-full h-full" />
