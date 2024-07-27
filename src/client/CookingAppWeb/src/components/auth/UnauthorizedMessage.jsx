674597a559d7d9e69685ea392a9fb7d527e9bb03
Georgi Markov <gogata1905@gmail.com>
2024-07-27 18:09:45 +0300

    good looking home

--- src/client/CookingAppWeb/src/components/auth/UnauthorizedMessage.jsx
+++ src/client/CookingAppWeb/src/components/auth/UnauthorizedMessage.jsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 
 export default function UnauthorizedMessage() {
   return (
-    <div className="flex flex-col justify-center items-center bg-customGray w-full h-screen relative">
+    <div className="flex flex-col justify-center items-center bg-customOrange w-full h-screen relative">
       {/* Header Section */}
-      <div className="absolute top-0 w-full flex justify-between items-center p-4 bg-customGray">
-        <Image src="/Main/icon2.png" alt="Logo" width={50} height={50} />
+      <div className="absolute top-0 w-full flex justify-between items-center p-4 bg-customOrange">
+        <Image src="/Main/icon2_dark.png" alt="Logo" width={50} height={50} />
         <nav className="flex space-x-6">
           <a href="#" className="text-customWhite hover:text-customGray400">
             Home
@@ -15,7 +15,7 @@ export default function UnauthorizedMessage() {
             Recipes
           </a>
           <a href="#" className="text-customWhite hover:text-customGray400">
-            Articles
+            Download
           </a>
           <a href="#" className="text-customWhite hover:text-customGray400">
             About Us
@@ -26,7 +26,7 @@ export default function UnauthorizedMessage() {
       <div className="text-center max-w-2xl mx-auto mt-40">
         <h1 className="text-5xl font-bold text-customWhite mb-6">The Easiest Way To Make Your Favorite Meal</h1>
         <p className="text-lg text-customWhite mb-12">Discover 1000+ recipes in your hand with the best recipe. Help you to find the easiest way to cook.</p>
-        <SignInButton text="Explore Recipes" />
+        <SignInButton text="Get Started" />
       </div>
     </div>
   );
