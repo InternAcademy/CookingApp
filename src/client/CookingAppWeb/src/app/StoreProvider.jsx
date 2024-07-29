2aa4d6c05d428ca966dd83e5fbe3e3b81db60834
Ekrem Beytula <ekremnptg@gmail.com>
2024-07-29 18:03:11 +0300

    Delete the existing project

--- src/client/CookingAppWeb/src/app/StoreProvider.jsx
+++
@@ -1,9 +0,0 @@
-// store/StoreProvider.jsx
-"use client";
-
-import { Provider } from "react-redux";
-import store from "@/store/page";
-
-export default function StoreProvider({ children }) {
-  return <Provider store={store}>{children}</Provider>;
-}
