104082e6b478fd46e142bbadfc9613d10ffdd0fb
Ekrem Beytula <ekremnptg@gmail.com>
2024-07-30 17:57:38 +0300

    Chat scrollable

--- src/client/CookingAppReact/src/pages/chat/Chat.jsx
+++ src/client/CookingAppReact/src/pages/chat/Chat.jsx
@@ -4,11 +4,113 @@ import logo from "/public/icon2.png";
 import ChatInput from "../../components/chatInput/ChatInput";
 export default function Chat() {
   return (
-    <section className="flex flex-col shrink">
+    <section className="flex flex-col overflow-hidden h-screen shrink">
       <Navbar />
-      <section className="flex flex-col content-between overflow-hidden flex-grow flex-wrap box-border">
+      <section className="flex flex-col content-between overflow-hidden flex-grow  box-border">
         {/* <img src={logo} alt="" /> */}
-        <ul className="overflow-y-auto flex flex-col flex-grow   justify-center items-center">
+        <ul className="overflow-y-auto flex flex-col    justify-center items-center">
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
+          <li>
+            <p className="text-3xl">text</p>
+          </li>
           <li>
             <p className="text-3xl">text</p>
           </li>
--- src/client/CookingAppReact/src/pages/layout/Layout.jsx
+++ src/client/CookingAppReact/src/pages/layout/Layout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "../../components/sidebar/Sidebar";
 export default function Layout() {
   return (
-    <main className="flex w-full overflow-x-hidden">
+    <main className="flex w-full overflow-hidden ">
       <Sidebar />
       <Outlet />
     </main>
