9775861f894fc271b7bfb76348e5363bd0ef9a96
Ekrem Beytula <ekremnptg@gmail.com>
2024-07-30 20:35:05 +0300

    Chat history items, chat template

--- src/client/CookingAppReact/src/pages/chat/Chat.jsx
+++ src/client/CookingAppReact/src/pages/chat/Chat.jsx
@@ -6,160 +6,118 @@ export default function Chat() {
   return (
     <section className="flex flex-col overflow-hidden h-screen shrink">
       <Navbar />
-      <section className="flex flex-col content-between overflow-hidden flex-grow  box-border">
+      <section className="flex flex-col content-between overflow-hidden flex-grow pt-5 shad  box-border">
         {/* <img src={logo} alt="" /> */}
-        <ul className="overflow-y-auto flex flex-col    justify-center items-center">
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-          <li>
-            <p className="text-3xl">text</p>
-          </li>
-        </ul>
+        <section className="w-full overflow-y-auto mb-20 grow flex justify-center">
+          <ul className=" flex flex-col w-2/4 gap-14 pb-10  justify-start  items-center">
+            <li className="w-4/5 flex  justify-end ">
+              <p className="bg-gray-100 rounded-2xl px-5 text-lg font-normal">
+                Hi there
+              </p>
+            </li>
+            <li className="w-4/5">
+              <div className="flex  justify-start items-start gap-6">
+                <img
+                  src={logo}
+                  alt=""
+                  width={35}
+                  className="bg-gray-200 rounded-2xl p-1"
+                />
+                <p>
+                  Hey there! How can I help you today? Hey there! How can I help
+                  you today? Hey there! How can I help you today? Hey there! How
+                  can I help you today? Hey there! How can I help you today? Hey
+                  there! How can I help you today?
+                </p>
+              </div>
+            </li>
+            <li className="w-4/5 flex  justify-end ">
+              <p className="bg-gray-100 rounded-2xl px-5 text-lg font-normal">
+                Hi there
+              </p>
+            </li>
+            <li className="w-4/5">
+              <div className="flex  justify-start items-start gap-6">
+                <img
+                  src={logo}
+                  alt=""
+                  width={35}
+                  className="bg-gray-200 rounded-2xl p-1"
+                />
+                <p>
+                  Hey there! How can I help you today? Hey there! How can I help
+                  you today? Hey there! How can I help you today? Hey there! How
+                  can I help you today? Hey there! How can I help you today? Hey
+                  there! How can I help you today?
+                </p>
+              </div>
+            </li>
+            <li className="w-4/5 flex  justify-end ">
+              <p className="bg-gray-100 rounded-2xl px-5 text-lg font-normal">
+                Hi there
+              </p>
+            </li>
+            <li className="w-4/5">
+              <div className="flex  justify-start items-start gap-6">
+                <img
+                  src={logo}
+                  alt=""
+                  width={35}
+                  className="bg-gray-200 rounded-2xl p-1"
+                />
+                <p>
+                  Hey there! How can I help you today? Hey there! How can I help
+                  you today? Hey there! How can I help you today? Hey there! How
+                  can I help you today? Hey there! How can I help you today? Hey
+                  there! How can I help you today?
+                </p>
+              </div>
+            </li>
+            <li className="w-4/5 flex  justify-end ">
+              <p className="bg-gray-100 rounded-2xl px-5 text-lg font-normal">
+                Hi there
+              </p>
+            </li>
+            <li className="w-4/5">
+              <div className="flex  justify-start items-start gap-6">
+                <img
+                  src={logo}
+                  alt=""
+                  width={35}
+                  className="bg-gray-200 rounded-2xl p-1"
+                />
+                <p>
+                  Hey there! How can I help you today? Hey there! How can I help
+                  you today? Hey there! How can I help you today? Hey there! How
+                  can I help you today? Hey there! How can I help you today? Hey
+                  there! How can I help you today?
+                </p>
+              </div>
+            </li>
+            <li className="w-4/5 flex  justify-end ">
+              <p className="bg-gray-100 rounded-2xl px-5 text-lg font-normal">
+                Hi there
+              </p>
+            </li>
+            <li className="w-4/5">
+              <div className="flex  justify-start items-start gap-6">
+                <img
+                  src={logo}
+                  alt=""
+                  width={35}
+                  className="bg-gray-200 rounded-2xl p-1"
+                />
+                <p>
+                  Hey there! How can I help you today? Hey there! How can I help
+                  you today? Hey there! How can I help you today? Hey there! How
+                  can I help you today? Hey there! How can I help you today? Hey
+                  there! How can I help you today?
+                </p>
+              </div>
+            </li>
+          </ul>
+        </section>
+
         <ChatInput />
       </section>
     </section>
