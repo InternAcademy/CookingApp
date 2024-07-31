be0ec60d3d4edcc2a52fd55ab6ba282594a0dd80
Ekrem Beytula <ekremnptg@gmail.com>
2024-07-31 17:36:38 +0300

    Navigate to empty chat

--- src/client/CookingAppReact/src/hooks/useSelectChat.js
+++ src/client/CookingAppReact/src/hooks/useSelectChat.js
@@ -3,62 +3,18 @@ import { useMutation } from "@tanstack/react-query";
 import { getChat } from "../http/chat";
 import { userActions } from "../store/userSlice";
 import { getToken } from "../msal/msal";
-
-const useSelectChat = () => {
-  const dispatch = useDispatch();
-
-  const { mutate } = useMutation({
-    mutationFn: getChat,
-    onSuccess: (response) => {
-      const { requests, responses } = response.data;
-      const minLength = Math.min(requests.length, responses.length);
-      let combinedArray = [];
-      for (let i = 0; i < minLength; i++) {
-        combinedArray.push({
-          type: requests[i].type,
-          content: requests[i].content,
-          role: "user",
-        });
-        combinedArray.push({
-          type: responses[i].type,
-          content: responses[i].content,
-          role: "bot",
-        });
-      }
-
-      for (let i = minLength; i < requests.length; i++) {
-        combinedArray.push({
-          type: requests[i].type,
-          content: requests[i].content,
-          role: "user",
-        });
-      }
-
-      for (let i = minLength; i < responses.length; i++) {
-        combinedArray.push({
-          type: responses[i].type,
-          content: responses[i].content,
-          role: "bot",
-        });
-      }
-      dispatch(
-        userActions.selectChat({
-          id: response.data.id,
-          content: combinedArray,
-        })
-      );
+import { useQuery } from "@tanstack/react-query";
+const useSelectChat = (chatId) => {
+  const { data, isPending, isError, error, refetch } = useQuery({
+    queryKey: ["getChat", chatId],
+    queryFn: async () => {
+      const token = await getToken();
+      return getChat({ token, chatId: chatId });
     },
+    enabled: false,
   });
 
-  const selectChat = async (chatId) => {
-    console.log(chatId);
-    const token = await getToken();
-    if (token) {
-      mutate({ token, chatId: chatId });
-    }
-  };
-
-  return selectChat;
+  return { data, isPending, isError, error, refetch };
 };
 
 export default useSelectChat;
--- src/client/CookingAppReact/src/pages/chat/Chat.jsx
+++ src/client/CookingAppReact/src/pages/chat/Chat.jsx
@@ -1,47 +1,51 @@
-import logo from "/public/icon2.png";
-import ChatInput from "../../components/chat/ChatInput";
-import useFetchUserStatus from "../../hooks/useFetchUserStatus";
 import { useSelector } from "react-redux";
-import UserMessage from "../../components/chat/UserMessage";
-import BotResponse from "../../components/chat/BotResponse";
-import Thinking from "../../components/chat/Thinking";
+import UserMessage from "@/components/chat/UserMessage";
+import BotResponse from "@/components/chat/BotResponse";
+import Thinking from "@/components/chat/Thinking";
+import useSelectChat from "@/hooks/useSelectChat";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { userActions } from "@/store/userSlice";
 export default function Chat() {
+  let { chatId } = useParams();
+  const dispatch = useDispatch();
+  const { data: response, refetch } = useSelectChat(chatId);
+  useEffect(() => {
+    if (chatId) {
+      refetch();
+    }
+  }, [chatId]);
+  useEffect(() => {
+    if (response) {
+      dispatch(userActions.selectChat(response));
+    }
+  }, [response]);
   const chat = useSelector((state) => state.user.selectedChat);
   const isThinking = useSelector((state) => state.ui.isThinking);
   const responseError = useSelector((state) => state.ui.responseError);
   const isOpenRecipes = useSelector((state) => state.ui.recipesOpen);
   const isOpenSideBar = useSelector((state) => state.ui.sidebarOpen);
-  useFetchUserStatus();
   return (
-    <section className="flex flex-col content-between overflow-hidden items-center flex-grow pt-5">
-      {!chat && (
-        <div className="grow pt-20">
-          <img src={logo} alt="" width={100} />
-        </div>
-      )}
-
-      {chat && (
-        <section className="w-full overflow-y-auto grow flex justify-center">
-          <ul
-            className={`flex flex-col gap-14 pb-10  justify-start  items-center
-            ${
-              isOpenRecipes || isOpenSideBar
-                ? "w-5/5 md:w-5/5 xl:w-4/5"
-                : "w-5/5 md:w-4/5 xl:w-3/5"
-            } `}
-          >
-            {chat.content.map((message) =>
-              message.role === "user" ? (
-                <UserMessage message={message} />
-              ) : (
-                <BotResponse message={message} />
-              )
-            )}
-            {isThinking && !responseError && <Thinking />}
-          </ul>
-        </section>
-      )}
-      <ChatInput />
+    <section className="w-full overflow-y-auto grow flex justify-center">
+      <ul
+        className={`flex flex-col gap-14 pb-10  justify-start  items-center
+    ${
+      isOpenRecipes || isOpenSideBar
+        ? "w-5/5 md:w-5/5 xl:w-4/5"
+        : "w-5/5 md:w-4/5 xl:w-3/5"
+    } `}
+      >
+        {chat &&
+          chat.content.map((message, index) =>
+            message.role === "user" ? (
+              <UserMessage key={index} message={message} />
+            ) : (
+              <BotResponse key={index} message={message} />
+            )
+          )}
+        {isThinking && !responseError && <Thinking />}
+      </ul>
     </section>
   );
 }
