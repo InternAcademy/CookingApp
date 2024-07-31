9e60b8cf902f4ac3063ba01215cab17d2d2ed600
Ekrem Beytula <ekremnptg@gmail.com>
2024-07-31 09:27:40 +0300

    On input change transition added

--- src/client/CookingAppReact/src/components/chatInput/ChatInput.jsx
+++ src/client/CookingAppReact/src/components/chatInput/ChatInput.jsx
@@ -1,23 +1,36 @@
 import { PaperClipIcon } from "@heroicons/react/24/outline";
 import { PaperAirplaneIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 export default function ChatInput() {
+  const [input, setInput] = useState("");
+
+  function handleChange(event) {
+    setInput(event.target.value);
+    console.log(event.target.value);
+  }
   return (
     <section className="flex items-center justify-center mb-5 w-full">
       <ul className="flex w-4/5 md:w-3/5 lg:w-2/5 items-center bg-gray-200 rounded-2xl gap-2  py-2 px-4">
         <li>
-          <PaperClipIcon className="size-6"/>
+          <PaperClipIcon className="size-6" />
         </li>
         <li className="w-full">
           <input
             type="text"
+            value={input}
             placeholder="What you wanna cook today?"
+            onChange={handleChange}
             className="w-full outline-none bg-gray-200 text-black placeholder-black"
           />
         </li>
         <li>
-          <PaperAirplaneIcon className="size-6" />
+          <PaperAirplaneIcon
+            className={`size-10 rounded-xl p-2 duration-200 ${
+              input.length > 0 ? "bg-orange-300" : ""
+            }`}
+          />
         </li>
       </ul>
     </section>
-  );
+  ); //size-10 rounded-xl p-2
 }
