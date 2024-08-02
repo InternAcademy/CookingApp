27a329402e89441eb51c53e139c678768a07068c
Ekrem Beytula <ekremnptg@gmail.com>
2024-08-02 15:34:29 +0300

    Recipes pagination

--- src/client/CookingAppReact/src/components/recipes/MyRecipes.jsx
+++ src/client/CookingAppReact/src/components/recipes/MyRecipes.jsx
@@ -10,9 +10,9 @@ import useFirstPageRecipes from "@/hooks/useFirstPageRecipes";
 
 export default function MyRecipes() {
   const isOpen = useSelector((state) => state.ui.recipesOpen);
-  const recipes = useSelector((state) => state.ui.filteredRecipes.recipes);
+  const recipesState = useSelector((state) => state.ui.filteredRecipes);
   const dispatch = useDispatch();
-  const { getFirstPageRecipes } = useFirstPageRecipes();
+  const { getFirstPageRecipes, loadMoreRecipes } = useFirstPageRecipes();
   function handleRecipes() {
     dispatch(uiActions.toggleRecipes());
   }
@@ -30,25 +30,47 @@ export default function MyRecipes() {
       getFirstPageAsync();
     }
   }, [isOpen]);
+  async function loadMore() {
+    const token = await getToken();
+    const decodedToken = jwtDecode(token);
 
-    return (
-        <section className={`bg-gray-100 flex flex-col flex-shrink-0 ${
+    loadMoreRecipes({
+      token: token,
+      userId: decodedToken.sub,
+      page: recipesState.page + 1,
+    });
+  }
+  return (
+    <section
+      className={`bg-gray-100 flex flex-col flex-shrink-0 ${
         isOpen ? "visible w-screen md:w-[25rem] md:relative" : "invisible w-0"
-      }  h-screen duration-300`}>
+      }  h-screen duration-300`}
+    >
+      <header
+        className={`flex flex-row justify-between items-center px-3 py-3 ${
+          isOpen ? "" : "hidden"
+        }`}
+      >
+        <ChevronLeftIcon
+          className="size-10  rounded-xl border border-gray-100  hover:border hover:border-gray-200 hover:cursor-pointer p-2"
+          onClick={handleRecipes}
+        />
+        <h1 className="text-lg">My Recipes</h1>
+        <UserIcon className="size-10 invisible md:visible rounded-xl border border-gray-100  hover:border hover:border-gray-200 hover:cursor-pointer p-2" />
+      </header>
 
-        <header className={`flex flex-row justify-between items-center px-3 py-3 ${isOpen ? "" : "hidden"}`}>
-            <ChevronLeftIcon 
-            className="size-10  rounded-xl border border-gray-100  hover:border hover:border-gray-200 hover:cursor-pointer p-2" 
-            onClick={handleRecipes}
-            />
-            <h1 className="text-lg">My Recipes</h1>
-            <UserIcon className="size-10 invisible md:visible rounded-xl border border-gray-100  hover:border hover:border-gray-200 hover:cursor-pointer p-2" />
-        </header>
-
-        <section className={`flex justify-center items-center px-4 py-4 h-28 ${isOpen ? "" : "hidden"}`}>
-            <input type="text" className="bg-white w-full h-3/4 rounded-xl outline-none px-6 py-3" placeholder="Looking for your favourite recipe?" />
-        </section>
-        <ul
+      <section
+        className={`flex justify-center items-center px-4 py-4 h-28 ${
+          isOpen ? "" : "hidden"
+        }`}
+      >
+        <input
+          type="text"
+          className="bg-white w-full h-3/4 rounded-xl outline-none px-6 py-3"
+          placeholder="Looking for your favourite recipe?"
+        />
+      </section>
+      <ul
         className={`
             overflow-y-scroll 
             overflow-x-hidden 
@@ -60,8 +82,11 @@ export default function MyRecipes() {
             ${isOpen ? "" : "hidden"}
             h-full`}
       >
-        {recipes.length > 0 &&
-          recipes.map((recipe) => <RecipeCard recipe={recipe} />)}
+        {recipesState.recipes.length > 0 &&
+          recipesState.recipes.map((recipe) => <RecipeCard recipe={recipe} />)}
+        {recipesState.page !== recipesState.totalPages && (
+          <button onClick={loadMore}>Load more...</button>
+        )}
       </ul>
     </section>
   );
