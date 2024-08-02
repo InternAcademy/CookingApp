06ee463eb71480cbb05fb811f9ea4e8619716faf
Ekrem Beytula <ekremnptg@gmail.com>
2024-08-02 18:17:42 +0300

    Update tokenFetcher.ts

--- src/client/CookingAppReact/src/msal/tokenFetcher.ts
+++ src/client/CookingAppReact/src/msal/tokenFetcher.ts
@@ -1,46 +1,46 @@
-import { PublicClientApplication } from "@azure/msal-browser";
+import {
+  PublicClientApplication,
+  InteractionRequiredAuthError,
+} from "@azure/msal-browser";
 
 export async function getCurrentToken(
   msalInstance: PublicClientApplication
 ): Promise<string | null> {
-  const acquireAccessToken = async () => {
-    const activeAccount = msalInstance.getActiveAccount(); // This will only return a non-null value if you have logic somewhere else that calls the setActiveAccount API
-    const accounts = msalInstance.getAllAccounts();
+  const accounts = msalInstance.getAllAccounts();
 
-    if (!activeAccount && accounts.length === 0) {
-      /*
-       * User is not signed in. Throw error or wait for user to login.
-       * Do not attempt to log a user in outside of the context of MsalProvider
-       */
-      return null;
-    }
+  if (accounts.length === 0) {
+    console.log("No accounts found in MSAL instance.");
+    return null;
+  }
 
-    const scopes =
-      "openid profile email api://1de79200-f5b3-4a56-8880-333cfddb121b/MealMaster.Default".split(
-        ","
-      ) || [];
+  const accountToUse = msalInstance.getActiveAccount() || accounts[0];
+  const scopes = [
+    "openid",
+    "profile",
+    "email",
+    "api://1de79200-f5b3-4a56-8880-333cfddb121b/MealMaster.Default",
+  ];
 
-    const request = {
-      scopes: scopes, // Ensuring scopes is an array of strings
-      account: activeAccount || accounts[0],
-    };
+  const request = { scopes, account: accountToUse };
 
-    try {
-      const authResult = await msalInstance.acquireTokenSilent(request);
-      return authResult.accessToken;
-    } catch (error) {
-      // If silent acquisition fails, try acquiring token through popup or redirect
+  try {
+    const authResult = await msalInstance.acquireTokenSilent(request);
+    return authResult.accessToken;
+  } catch (error) {
+    if (error instanceof InteractionRequiredAuthError) {
+      console.log(
+        "Silent token acquisition failed, user interaction required."
+      );
       try {
         const authResult = await msalInstance.acquireTokenPopup(request);
         return authResult.accessToken;
-      } catch (error) {
-        console.error("Error acquiring token:", error);
+      } catch (popupError) {
+        console.error("Popup failed: ", popupError);
         return null;
       }
+    } else {
+      console.error("Token acquisition failed: ", error);
+      return null;
     }
-  };
-
-  var token = await acquireAccessToken();
-
-  return token;
+  }
 }
