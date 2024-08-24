bd8fc92cafef96b518277fcd546448ab8d8a95fc
David Hristov <orionvt3@gmail.com>
2024-08-24 12:13:06 +0300

    added frontend and backend validations for image type and size

---
+++ src/server/CookingApp/Infrastructure/Exceptions/InvalidImageUploadException.cs
@@ -0,0 +1,15 @@
+﻿namespace CookingApp.Infrastructure.Exceptions
+{
+    public class InvalidImageUploadException : Exception
+    {
+        public InvalidImageUploadException()
+        {
+
+        }
+
+        public InvalidImageUploadException(string message) : base(message)
+        {
+
+        }
+    }
+}
